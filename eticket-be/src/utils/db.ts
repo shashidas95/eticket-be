32c1cc853fa57813e4f1ed1c1ab17510bb80dda9
shashidas95 <shashidas95@gmail.com>
2024-10-05 21:33:22 +0600

    h

--- eticket-be/src/utils/db.ts
+++ eticket-be/src/utils/db.ts
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
-import logger from "./logger";
+import mongoose from 'mongoose'
+import logger from './logger'
 
-require("dotenv").config();
+require('dotenv').config()
 
-const dbUrl: string = process.env.DB_URL || "";
+const dbUrl: string = process.env.DB_URL || ''
 export const connectDb = async () => {
   try {
     await mongoose.connect(dbUrl).then(() => {
-      logger.info(`MongoDB connected!!!!.`);
-    });
+      logger.info(`MongoDB connected!!.`)
+    })
   } catch (err: any) {
-    logger.error(err.message);
-    setTimeout(() => connectDb, 5000);
+    logger.error(err.message)
+    setTimeout(() => connectDb, 5000)
   }
-};
+}
